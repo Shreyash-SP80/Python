4731636634e30a7a75028485743623f05f03b931
Shreyash-SP80 <cyberninjax45@gmail.com>
2025-07-03 22:39:43 +0530

    02 User Input

---
+++ 02_User Input/1. Basic  userinput.py
@@ -0,0 +1,17 @@
+# User Input:
+# - Python provides simple ways to get input from users. The most common method is using the built-in input() function.
+
+# The input() function reads a line from the user (as a string).
+
+# Syntax:
+# user_input = input("Prompt message: ")
+# Displays an optional prompt message.
+# Returns the user's input as a string.
+
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+
+# Output:
+# Enter your name: Shreyash
+# Hello, Shreyash!
+
